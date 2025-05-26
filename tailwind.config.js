68114dbf3e87fa3d354b3c4bc36cee785facc0d1
cheng <hmcbook@163.com>
2025-05-27 00:07:18 +0800

    feat: 完成关键对话助手基础功能开发

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'base': '16px', // 设置基础字体大小
+      },
+    },
   },
   plugins: [],
 };
